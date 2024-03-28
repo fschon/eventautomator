9e377c15c15fb7e8718faaa8b0689f11fee97fa5
fschon <franz@schoning.com>
2024-03-28 14:54:17 +0100

    Added hilla react components

--- src/main/frontend/generated/views.ts
+++
@@ -1,5 +0,0 @@
-import { createRoute } from "@vaadin/hilla-file-router/runtime.js";
-import * as Page0 from "../views/counter.js";
-import * as Page1 from "../views/reactvideo.js";
-const routes = createRoute("", [createRoute("counter", Page0), createRoute("reactvideo", Page1)]);
-export default routes;
